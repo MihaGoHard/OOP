4ef472f9cf682b52b773984f826fa8cfc5954b06
MihaGoHard <shasherin06@bk.ru>
2021-02-15 10:50:34 +0300

    parsed args

--- lab_1/CopyFile/CopyFile.cpp
+++ lab_1/CopyFile/CopyFile.cpp
@@ -1,9 +1,32 @@
 #include <fstream>
 #include <iostream>
+#include <optional>
+#include <string>
+
+
+struct Args
+{
+	std::string inputFileName;
+	std::string outputFileName;
+};
+
+std::optional<Args> ParseArgs(int argc, char* argv[]) // возвращает либо объект с аргуметами либо nullopt
+{
+	if (argc != 3)
+	{
+		return std::nullopt;
+	}
+	Args args;
+	args.inputFileName = argv[1];
+	args.outputFileName = argv[2];
+	return args;
+}
+
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	auto args = ParseArgs(argc, argv);
+	if (!argc)
 	{
 		std::cout << "Invalid arguments number \n";
 		std::cout << "Usage: CopyFile.exe <input file name> <output file name>";
@@ -11,18 +34,18 @@ int main(int argc, char* argv[])
 	}
 
 	std::ifstream input;
-	input.open(argv[1]);
+	input.open(args->inputFileName); // через стрелку обращение к значению объекта(убедиться, что объект не пуст)
 	if (!input.is_open())
 	{
-		std::cout << "Faild to open '" << argv[1] << "' for reading\n";
+		std::cout << "Faild to open '" << args->inputFileName << "' for reading\n";
 		return 1;
 	}
 
 	std::ofstream output;
-	output.open(argv[2]);
+	output.open(args->outputFileName);
 	if (!output.is_open())
 	{
-		std::cout << "Faild to open '" << argv[2] << "' for writing\n";
+		std::cout << "Faild to open '" << args->outputFileName << "' for writing\n";
 		return 1;
 	}
 
@@ -35,13 +58,13 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	if (input.bad())
+	if (input.bad())   
 	{
 		std::cout << "Faild to read data from input file\n"; 
 		return 1;
 	}
 
-	if (!output.flush())
+	if (!output.flush())  // connect with buffer and disc 
 	{
 		std::cout << "faild to write data to otput file\n";
 		return 1;
