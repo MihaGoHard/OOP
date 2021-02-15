85828b08b9decb725a503dd2e2a157bebe4b7c35
MihaGoHard <shasherin06@bk.ru>
2021-02-15 09:17:20 +0300

    added args num & input file exist checking

--- lab_1/CopyFile/CopyFile.cpp
+++ lab_1/CopyFile/CopyFile.cpp
@@ -3,8 +3,22 @@
 
 int main(int argc, char* argv[])
 {
+	if (argc != 3)
+	{
+		std::cout << "Invalid arguments number \n";
+		std::cout << "Usage: CopyFile.exe <input file name> <output file name>";
+		return 1;
+	}
+
     std::ifstream input;
     input.open(argv[1]);
+
+	if (!input.is_open())
+	{
+		std::cout << "Faild to open '" << argv[1] << "' for reading\n";
+		return 1;
+	}
+
 	char ch;
 	while (input.get(ch))
 	{
