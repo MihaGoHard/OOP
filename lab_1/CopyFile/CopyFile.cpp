5e0a27f126410ba0318c90f8f40c8c1d16aa3c2c
MihaGoHard <shasherin06@bk.ru>
2021-02-15 09:03:14 +0300

    read from file

--- lab_1/CopyFile/CopyFile.cpp
+++ lab_1/CopyFile/CopyFile.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << argc << "\n";
+    std::ifstream input;
+    input.open(argv[1]);
+	char ch;
+	while (input.get(ch))
+	{
+		std::cout.put(ch);
+	}
+
+	/*std::cout << argc << "\n";
     for (int i = 0; i < argc; ++i)
     {
         std::cout << argv[i] << "\n";
-    }
+    }*/
     return 0;
 }
-
